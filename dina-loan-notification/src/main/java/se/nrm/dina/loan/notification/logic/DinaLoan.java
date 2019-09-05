44809c2c2dd3acccffefee57b8bd665e481d6c57
idali0226 <ida.li@nrm.se>
2019-09-05 14:26:00 +0200

    Fix bugs

--- dina-loan-notification/src/main/java/se/nrm/dina/loan/notification/logic/DinaLoan.java
+++ dina-loan-notification/src/main/java/se/nrm/dina/loan/notification/logic/DinaLoan.java
@@ -3,8 +3,9 @@
  * and open the template in the editor.
  */
 package se.nrm.dina.loan.notification.logic;
-
+ 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,14 +13,13 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;    
+import org.slf4j.LoggerFactory;
 import se.nrm.dina.datamodel.impl.Agent;
 import se.nrm.dina.datamodel.impl.Loan;
 import se.nrm.dina.datamodel.impl.Loanagent;
 import se.nrm.dina.loan.notification.dao.DinaDao;
-import se.nrm.dina.loan.notification.email.DinaMail; 
+import se.nrm.dina.loan.notification.email.DinaMail;
 import se.nrm.dina.loan.notification.vo.OverdueLoan;
- 
 
 /**
  *
@@ -27,40 +27,19 @@ import se.nrm.dina.loan.notification.vo.OverdueLoan;
  */
 @Stateless
 public class DinaLoan {
-    
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+    private static final String PREPARED = "preparer";
+    private static final String PRIMARY_BORROWER = "borrower primary";
+    private static final String SECONDARY_BORROWER = "borrower secondary";
+    private static final String BORROWER = "borrower";
+
     @EJB
-    private DinaDao dao; 
-    
+    private DinaDao dao;
+
     @Inject
     private DinaMail mail;
-
-    private Agent preparedBy;
-    private Agent primaryBorrower;
-    private Agent secondaryBorrower;
-    private Agent borrower;
-  
-    private void setAgentsType(Loanagent loanAgent) {
-        Agent agent = loanAgent.getAgentID();
-        String role = loanAgent.getRole().trim();
-        switch (role) {
-            case "preparer":
-                preparedBy = agent;
-                break;
-            case "borrower primary": 
-                primaryBorrower = agent;
-                break;
-            case "borrower secondary":
-                secondaryBorrower = agent;
-                break;
-            case "borrower":
-                borrower =agent;
-                break;
-        }
-
-    }
-
+ 
     public void getOverDueLoan() {
         logger.info("getOverDueLoan");
 
@@ -68,31 +47,60 @@ public class DinaLoan {
         Map<String, List<OverdueLoan>> map = new HashMap<>();
 
         loans.stream()
-                .forEach(l -> {
-                    l.getLoanagentList().stream()
-                    .forEach(a -> {
-                        setAgentsType(a);
-                    });
+                .forEach(loan -> {
+                    Agent preparedAgent;
+                    Agent primary = null;
+                    Agent secondary = null;
 
-                    if (primaryBorrower == null) {
-                        primaryBorrower = secondaryBorrower == null ? borrower == null ? null : borrower : secondaryBorrower;
-                        secondaryBorrower = null;
-                    }
+                    List<Loanagent> loanAgents = loan.getLoanagentList();
 
-                    if (preparedBy != null) {
-                        OverdueLoan voerDueLoan = new OverdueLoan(l.getLoanNumber(), l.getCurrentDueDate(), l.getLoanDate(), primaryBorrower, secondaryBorrower, preparedBy);
-                        List<OverdueLoan> list = new ArrayList<>();
-                         
-                        if(preparedBy.getEmail() != null) {
-                            if (map.containsKey(preparedBy.getEmail())) {
-                                list = map.get(preparedBy.getEmail()); 
-                            } 
-                            list.add(voerDueLoan);
-                            map.put(preparedBy.getEmail(), list); 
+                    Loanagent prepared = loanAgents.stream()
+                            .filter(a -> a.getRole().equals(PREPARED))
+                            .findFirst().orElse(null);
+
+                    Loanagent primaryBorrower = loanAgents.stream()
+                            .filter(a -> a.getRole().equals(PRIMARY_BORROWER))
+                            .findFirst().orElse(null);
+
+                    Loanagent secondaryBorrower = loanAgents.stream()
+                            .filter(a -> a.getRole().equals(SECONDARY_BORROWER))
+                            .findFirst().orElse(null);
+
+                    Loanagent borrower = loanAgents.stream()
+                            .filter(a -> a.getRole().equals(BORROWER))
+                            .findFirst().orElse(null);
+
+                    if (primaryBorrower != null) {
+                        primary = primaryBorrower.getAgentID();
+                        if (secondaryBorrower != null) {
+                            secondary = secondaryBorrower.getAgentID();
+                        } else if (borrower != null) {
+                            secondary = borrower.getAgentID();
                         }
-                        
+                    } else if (secondaryBorrower != null) {
+                        primary = secondaryBorrower.getAgentID();
+                    } else if (borrower != null) {
+                        primary = borrower.getAgentID();
                     }
-                });  
-        mail.sendMail(map); 
-    } 
+
+                    if (prepared != null) { 
+                        String loanNumber = loan.getLoanNumber();
+                        Date currentDueDate = loan.getCurrentDueDate();
+                        Date loanDate = loan.getLoanDate();
+                        preparedAgent = prepared.getAgentID();
+                        OverdueLoan voerDueLoan = new OverdueLoan(loanNumber, 
+                                currentDueDate, loanDate, primary, secondary, preparedAgent);
+                        List<OverdueLoan> list = new ArrayList<>();
+                        String email = preparedAgent.getEmail();
+                        if (email != null) {
+                            if (map.containsKey(email)) {
+                                list = map.get(email);
+                            }
+                            list.add(voerDueLoan);
+                            map.put(email, list);
+                        } 
+                    }
+                }); 
+        mail.sendMail(map);
+    }
 }
